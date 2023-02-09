28210a09d0173a5a0686db1ebfa077d0a623f4ac
Nageshwar Yadav <95100082+Ranveer098@users.noreply.github.com>
2023-02-09 22:04:56 +0530

    hoop_jump

---
+++ hoop_jump.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t,a,d;
+  cin>>t;
+  while (t--)
+  {
+    cin>>a;
+    if(a==1){
+      cout<<a<<endl;
+
+    }
+    else{
+      cout<<(a/2)+1<<endl;
+    }
+  }
+  
+}
